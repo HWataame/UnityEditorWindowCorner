f771d7a00958a2d917eded612e48767d7dafc6a9
hWataame <151874689+HWataame@users.noreply.github.com>
2025-07-21 23:38:38 +0900

    feat: dllのエントリーポイントを追加

---
+++ ExternalLibraries/UnityEditorWindowCorner/UnityEditorWindowCorner/Sources/Main.cpp
@@ -0,0 +1,26 @@
+/*
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+UnityEditorから呼び出す関数を保持するソースファイル
+
+Main.cpp
+────────────────────────────────────────
+バージョン: 1.0.0
+2025 Wataame(HWataame)
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+*/
+#include "ShowWindowReplacer.h"
+#include "Corner/WindowCorner.h"
+using namespace HW::UnityEditorWindowCorner::Corner;
+
+namespace HW::UnityEditorWindowCorner
+{
+    /// <summary>
+    /// UnityEditorの初期化時の処理
+    /// </summary>
+    /// <returns>処理結果</returns>
+    bool OnUnityEditorInitialize() noexcept
+    {
+
+        return true;
+    }
+}
